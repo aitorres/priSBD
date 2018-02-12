6fdd4509973953e064e02f33d0c8d4342b3f70e8
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-02-12 15:12:17 -0400

    Minimal configuration

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Access to APIs
-currency_api_url = ''
-sbd_api_url = ''
-steemd_nodes = ''
+currency_api_url = 'https://api.fixer.io/latest?base=USD'
+sbd_api_url = 'https://api.coinmarketcap.com/v1/ticker/steem-dollars/'
+steemd_nodes = ['https://steemd.pevo.science']
 
 # Steemit user config
 # Leave blank if you don't want to use user features
