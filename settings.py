8fdab4151227edcc984791920e2383c6dea9e2d8
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-02-12 03:01:22 -0400

    Added separate settings module

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Access to APIs
+currency_api_url = ''
+sbd_api_url = ''
+steemd_nodes = ''
+
+# Steemit user config
+# Leave blank if you don't want to use user features
+username = ''
+posting_key = ''
